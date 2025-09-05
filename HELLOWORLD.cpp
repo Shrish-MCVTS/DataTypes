1b44af329403f750fa350f939a164a4cd790fa61
Shrish-MCVTS <Shrish.kushwaha@mcvts.org>
2025-09-05 08:56:00 -0400

    Add files via upload

---
+++ HELLOWORLD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 15;
+    float bankbalance = 20988.6;
+    double score = 67.67;
+    char grade = 'A';
+    bool under21 = true;
+    string  qoute = "Comp-sci is life";
+    cout << age << endl;
+    cout << bankbalance << endl;
+    cout << score << endl;
+    cout << grade << endl;
+    cout << under21 << endl;
+    cout << qoute << endl;
+}
